0e5bc5e516ef05ca810d29d0426102636d870353
future0143 <125833563+future0143@users.noreply.github.com>
2023-02-20 19:13:37 +0200

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class calculator {
+public class Main {
     public static void main(String[] args) throws IOException {
         Scanner console=new Scanner (System.in);
         System.out.println("Input:");
@@ -13,22 +13,22 @@ public class calculator {
 
     }
     public static String calc(String input) throws IOException {
-        String [] str = new String[3];    //создаю массив из 3 переменных
-        str = input.split(" ");     //в массив помещаю введенную строку, разделенную на элементы пробелами
+        String [] str = new String[3];    
+        str = input.split(" ");     
 
-        if (str.length>3)                //проверка на количество введенных переменных (2 переменные и 1 оператор)
+        if (str.length>3)                
             throw new IOException();
 
-        int a = Integer.parseInt(str[0]);   //преобразование элемента массива (типа String) в число
+        int a = Integer.parseInt(str[0]);   
         String operator = str[1];
         int b = Integer.parseInt(str[2]);
         int result = 0;
 
-        if ((a < 1) && (a > 10) && (b < 1) && (b > 10)) {    //проверка на диапазон от 1 до 10
+        if ((a < 1) && (a > 10) && (b < 1) && (b > 10)) {    
             throw new IOException();
         }
         if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
-            throw new IOException();         // проверка на введенный оператор (разрешен ввод только 4-х)
+            throw new IOException();         
         }
         switch (operator) {
                 case "+":
